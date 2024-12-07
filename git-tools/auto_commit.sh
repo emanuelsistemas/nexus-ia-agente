3cb1227d0638fda1da69a5cb7f113e2eaa484d62
emanuel <emanuel.sistemas@gmail.com>
2024-12-07 12:34:40 +0000

    Commit automático - 07/12/2024 12:34:40 - Hash: e28fd93

--- git-tools/auto_commit.sh
+++ git-tools/auto_commit.sh
@@ -6,13 +6,13 @@ DATA_HORA=$(date '+%d/%m/%Y %H:%M:%S')
 # Adiciona todas as alterações
 git add .
 
-# Faz o commit inicial
-git commit -m "Commit automático - $DATA_HORA"
+# Cria um commit temporário para obter o hash
+TEMP_COMMIT=$(git commit -m "temp" --no-verify)
 
-# Obtém o hash curto do commit que acabamos de criar
+# Obtém o hash curto do commit temporário
 HASH=$(git rev-parse --short HEAD)
 
-# Atualiza a mensagem do commit com o hash curto
+# Faz o commit final com o hash na mensagem
 git commit --amend -m "Commit automático - $DATA_HORA - Hash: $HASH" --no-verify
 
 # Faz o push
