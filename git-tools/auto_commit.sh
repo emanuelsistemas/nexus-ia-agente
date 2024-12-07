512eb79f3535e2defb00a1f895ff404f036afe6b
emanuel <emanuel.sistemas@gmail.com>
2024-12-07 12:38:04 +0000

    Commit automático - 07/12/2024 12:38:04

--- git-tools/auto_commit.sh
+++ git-tools/auto_commit.sh
@@ -13,10 +13,10 @@ git commit -m "Commit automático - $DATA_HORA" --no-verify
 HASH=$(git rev-parse --short HEAD)
 
 # Cria o diretório de logs se não existir
-mkdir -p ../logs
+mkdir -p ./logs
 
-# Adiciona o registro no arquivo de log
-echo "[$DATA_HORA] Commit: $HASH - Para restaurar use: git reset --hard $HASH" >> ../logs/commit_history.log
+# Adiciona o registro no arquivo de log dentro da pasta git-tools
+echo "[$DATA_HORA] Commit: $HASH - Para restaurar use: git reset --hard $HASH" >> ./logs/commit_history.log
 
 # Faz o push
 git push origin HEAD
@@ -24,4 +24,4 @@ git push origin HEAD
 # Exibe a mensagem de confirmação
 echo "Commit realizado com sucesso!"
 echo "Hash para restauração: $HASH"
-echo "Este hash foi salvo em ../logs/commit_history.log"
+echo "Este hash foi salvo em ./logs/commit_history.log"
